e184966db2ccb0a9b8c05d56a621c4f56b794988
valikhan03 <valikhan.eltaev@gmail.com>
2023-04-01 18:33:13 +0600

    tb_attempt_requests fix

--- attempt.sql
+++ attempt.sql
@@ -10,6 +10,7 @@ create table tb_attempt_requirements(
 create table tb_attempt_requests(
     id number not null unique,
     auction_id UUID not null,
+    user_id number not null,
     approved number default 0,
     enter_fee_payed number default 0
 );
