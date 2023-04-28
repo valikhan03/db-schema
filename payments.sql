da59293e120d59c37c9c9f530f89d2876b7c2020
valikhan03 <valikhan.eltaev@gmail.com>
2023-04-29 01:51:14 +0600

    invoices and payments added

--- payments.sql
+++ payments.sql
@@ -6,4 +6,24 @@ create table tb_bids(
 );
 
 
+create table tb_invoices(
+    id serial UNIQUE,
+    user_id integer REFERENCES tb_users(id),
+    product_type integer,
+    product_id integer,
+    product_name text,
+    amount float,
+    currency text,
+    p_date_insert date,
+    p_date_update date,
+    p_is_payed integer default 0
+);
 
+
+create table tb_payments(
+    id serial UNIQUE,
+    user_id integer REFERENCES tb_users(id),
+    invoice_id integer REFERENCES tb_invoices(id),
+    payed_amount integer,
+    currency text
+);
